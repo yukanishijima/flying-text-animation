a08c1d8945b6812c1a8844a0610b9d7b6d282f99
Yuka Nishijima <48897211+yukanishijima@users.noreply.github.com>
2021-04-25 23:41:17 -0400

    Add gsap animation for text 1 & 2

--- script.js
+++ script.js
@@ -1,62 +1,38 @@
+// .text_1
 const textOne = document.querySelector(".text_1");
-textOne.innerHTML = textOne.textContent.replace(/\S/g, "<span>$&</span>"); // match anything but white space
+textOne.innerHTML = textOne.textContent.replace(/\S/g, "<span class='letter_1'>$&</span>"); // match anything but white space
+
+// .text_2
 const textTwo = document.querySelector(".text_2");
-textTwo.innerHTML = textTwo.textContent.replace(/\S/g, "<span>$&</span>");
+textTwo.innerHTML = textTwo.textContent.replace(/\S/g, "<span class='letter_2'>$&</span>");
 
-const animationOne = anime.timeline({
-	targets: ".text_1 span",
-	loop: true,
-});
+// .text_3
+// const textThree = document.querySelector(".text_3");
+// textThree.innerHTML = textThree.textContent.replace(/\S/g, "<span class='letter_3'>$&</span>");
 
-animationOne
-	.add(
-		{
-			translateY: [-200, 0],
-			translateX: (el, i) => {
-				if (i == 0) {
-					return [-300, 0];
-				} else if (i == 4) {
-					return [300, 0];
-				}
-			},
-			scale: [14, 1],
-			opacity: [0, 1],
-			easing: "easeOutCirc",
-			duration: 500,
-			delay: (el, i) => 500 * i,
-		},
-		1200 // absolute offset
-	)
-	.add({
-		translateY: 500,
-		easing: "easeInBack",
-		duration: 800,
-		delay: anime.stagger(100, { start: 400 }),
-	});
+// .text_2 - create an array of letters in a random order
+const RandomLetter = [];
+const letters = document.querySelectorAll(".letter_2");
+while (RandomLetter.length < letters.length) {
+	let r = Math.floor(Math.random() * letters.length) + 1;
+	let letter = `.letter_2:nth-child(${r})`;
+	if (RandomLetter.indexOf(letter) === -1) RandomLetter.push(letter);
+}
 
-const animationTwo = anime.timeline({
-	targets: ".text_2 span",
-	loop: true,
-});
+const tl = gsap.timeline();
 
-animationTwo
-	.add({
-		translateX: function () {
-			return anime.random(-500, 500);
-		},
-		translateY: function () {
-			return anime.random(-500, 500);
-		},
-		opacity: 0,
-		duration: 2000,
-		delay: anime.stagger(5, { start: 500 }),
-	})
-	.add({
-		rotate: 0,
-		translateX: 0,
-		translateY: 0,
-		duration: 20,
-	})
-	.add({
-		opacity: 1,
-	});
+tl.set(".letter_1", { opacity: 0 });
+tl.set(".text_2", { display: "none" });
+tl.set(".text_3", { display: "none" });
+
+// fly in from the front and drop down
+tl.from(".letter_1", { duration: 0.5, opacity: 0, scale: 10, y: -200, stagger: 0.5 });
+tl.to(".letter_1", { duration: 1.5, opacity: 0, y: 300, ease: "back.inOut(4)", stagger: 0.1, display: "none" }, "+=0.4");
+
+// appear a letter one by one and disappear all
+tl.to(".text_2", { display: "flex" });
+tl.from(RandomLetter, { duration: 0.5, opacity: 0, stagger: 0.1 });
+tl.to(".text_2", { opacity: 0, display: "none" }, "+=0.4");
+
+// WIP
+tl.to(".text_3", { display: "flex" });
